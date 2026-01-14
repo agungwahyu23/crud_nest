2cc023ca4490bc1fbd5a8ff02391df21a9f3f5e0
agungwahyu23 <agungwahyu23699@gmail.com>
2026-01-14 08:05:55 +0700

    feat: swagger

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ZodValidationPipe } from './common/pipes/zod.validation.pipe';
 import { ResponseInterceptors } from './common/interceptors/response.interceptors';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,7 +13,17 @@ async function bootstrap() {
 
   //global exception filter
   app.useGlobalFilters(new HttpExceptionFilter());
-  
+
+  // swagger
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   app.useGlobalPipes(new ZodValidationPipe());
   await app.listen(process.env.PORT ?? 3000);
 }
